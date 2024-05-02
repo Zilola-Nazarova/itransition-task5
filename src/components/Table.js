23c438b139f5136c60dbbe97a68f1db632ea8260
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-02 19:19:31 +0500

    Create helper function that creates random users, change the table to render 20 random users using helper function

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,36 @@
-const Navbar = () => (
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <td>#</td>
-          <td>ID:</td>
-          <td>Name:</td>
-          <td>Address:</td>
-          <td>Phone number:</td>
-        </tr>
-      </thead>
-      <tbody />
-    </table>
-  </div>
-);
+import { faker } from '@faker-js/faker';
+import createRandomUser from '../helpers/faker';
 
-export default Navbar;
+const Table = () => {
+  const users = faker.helpers.multiple(createRandomUser, {
+    count: 20,
+  });
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>#</td>
+            <td>ID:</td>
+            <td>Name:</td>
+            <td>Address:</td>
+            <td>Phone number:</td>
+          </tr>
+        </thead>
+        <tbody>
+          { users.map((user, i) => (
+            <tr key={user.userId}>
+              <td>{i + 1}</td>
+              <td>{user.userId}</td>
+              <td>{user.firstName} {user.middleName} {user.lastName}</td>
+              <td>{user.number}</td>
+              <td>{user.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
---
+++ src/helpers/faker.js
@@ -0,0 +1,14 @@
+import { faker } from '@faker-js/faker';
+
+function createRandomUser() {
+  return {
+    userId: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    middleName: faker.person.middleName(),
+    lastName: faker.person.lastName(),
+    number: faker.phone.number(),
+    address: faker.date.streetAddress({ useFullAddress: true }),
+  };
+}
+
+export default createRandomUser;
