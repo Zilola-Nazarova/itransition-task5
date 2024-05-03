7b25e94c02a09745c1655e8b272d08a6bcd5ac4b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-03 22:23:11 +0500

    Create slider and input to set the errors variable to store

--- src/components/Errors.js
+++ src/components/Errors.js
@@ -1,5 +1,43 @@
-const Errors = () => (
-  <button type="button">Errors:</button>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { set } from '../redux/settings/errorsSlice';
+
+const Errors = () => {
+  const errors = useSelector((state) => state.errors.value);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log(`Now errors is ${errors}`);
+  }, [errors]);
+
+  return (
+    <>
+      <label htmlFor="errors" className="form-label">
+        Errors:
+        <input
+          type="range"
+          className="form-range"
+          min="0"
+          max="10"
+          step="1"
+          id="errors"
+          value={errors > 10 ? 10 : errors}
+          onChange={(e) => {
+            dispatch(set(e.target.value));
+          }}
+        />
+        <input
+          type="number"
+          className="form-control"
+          min="0"
+          max="1000"
+          value={errors}
+          onChange={(e) => {
+            dispatch(set(e.target.value));
+          }}
+        />
+      </label>
+    </>
+  );
+};
 
 export default Errors;
