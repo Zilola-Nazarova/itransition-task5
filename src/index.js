7d1b211a2f4395cb7d45754ebea2e24a95bdbd5f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-04 18:30:21 +0500

    Install tanstack/react-query and react-intersection-observer dependencies, set up queryClient, add propTypes to dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import store from './redux/store';
 import './index.css';
 import App from './components/App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 );
